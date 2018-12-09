62f8be30e1c7f39ca579a56ee287d0bd772f4a16
Vili <vili_mihovska@live.com>
2018-12-09 17:38:55 +0200

    some tests

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => res.json({ username: 'Flavio' }));
+
+app.get('/uppercase/:theValue', (req, res) => res.send(req.params.theValue.toUpperCase()));
+
+
+
+
 app.listen(3000, () => console.log('Server ready'));
